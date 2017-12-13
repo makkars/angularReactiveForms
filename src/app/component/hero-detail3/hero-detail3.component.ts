88b1ae3f2abd16caec0e73d2eda5c1ce4efb1629
eshmakk <shashank.makkar@ericsson.com>
2017-12-13 23:03:49 +0530

    Validators in FB

--- src/app/component/hero-detail3/hero-detail3.component.ts
+++ src/app/component/hero-detail3/hero-detail3.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-hero-detail3',
@@ -12,7 +12,7 @@ export class HeroDetail3Component implements OnInit {
 
   createForm(): any {
     this.heroForm = this.fb.group({
-      name: ''
+      name: ['', Validators.required]
     });
   }
 
